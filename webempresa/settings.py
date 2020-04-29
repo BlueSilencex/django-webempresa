d9dc1b9cd524241012a98ece0b4d98c6788d4eb0
Blue <mauricio.e.b.g@hotmail.com>
2020-04-29 18:06:47 -0500

    Version en desarrollo 0.2

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
     'core',
-    'services',
+    'services.apps.ServicesConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
